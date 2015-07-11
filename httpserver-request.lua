bf340f63e1697792d3f2e8d3d8891a0790e5fb31
Seppel Hardt <sebasth@gmx.de>
2015-07-11 12:13:33 +0200

    Update httpserver-request.lua

--- httpserver-request.lua
+++ httpserver-request.lua
@@ -9,7 +9,7 @@ local function validateMethod(method)
 end
 
 local function uriToFilename(uri)
-   return "http/" .. string.sub(uri, 2, -1)
+   return "http_" .. string.sub(uri, 2, -1)
 end
 
 local function parseArgs(args)
